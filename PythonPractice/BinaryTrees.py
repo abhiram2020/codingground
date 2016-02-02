22546e8e0b7845596e797cd3fb343f37103a02ff
abhiram2020 <abhiram2020@users.noreply.github.com>
2016-02-02 09:48:41 -0800

    Update BinaryTrees.py
    identicalTrees() Prints if Both the Trees are Identical

--- PythonPractice/BinaryTrees.py
+++ PythonPractice/BinaryTrees.py
@@ -6,17 +6,44 @@ class node:
         self.left = None
         self.right = None
 
-#Function to Determine the Size of the Tree
 def treeSize(node):
     if node is None:
         return 0
     return treeSize(node.left) + 1 + treeSize(node.right)
 
-#Form the Tree
-root = node(1)
-root.left = node(2)
-root.right = node(3)
-root.left.left = node(4)
-root.left.right = node(5)
+def identicalTrees(node_a,node_b):
+    #if both the Nodes are NULL, Trees are Identical
+    if node_a is None and node_b is None:
+        return True
+    #if both are not NULL, Compare the DATA and recurse over the
+    #Child Trees
+    if (node_a is not None and node_b is not None):
+        return ((node_a.data == node_b.data) and
+                identicalTrees(node_a.left,node_b.left) and
+                identicalTrees(node_a.right,node_b.right))
+    #if any one of them is NULL
+    #return FALSE
+    return False
 
-print "Tree Size: ",treeSize(root)
+
+root1 = node(1)
+root1.left = node(2)
+root1.right = node(3)
+root1.left.left = node(4)
+root1.left.right = node(5)
+
+root2 = node(1)
+root2.left = node(2)
+root2.right = node(3)
+root2.left.left = node(4)
+root2.left.right = node(5)
+
+print "Tree Size: ",treeSize(root1)
+
+if(identicalTrees(root1,root2)):
+    print "Identical TREES"
+else:
+    print "NON Identical TREES"
+    
+
+        
