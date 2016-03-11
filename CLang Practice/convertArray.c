109681868e5d48a39abef3ca8101f6d9effdcfab
abhiram2020 <abhiram2020@users.noreply.github.com>
2016-03-11 11:19:00 -0800

    Create convertArray.c
    Given an array:
    
    [a_1, a_2, ..., a_N, b_1, b_2, ..., b_N, c_1, c_2, ..., c_N ]  
    
    convert it to:
    
    [a_1, b_1, c_1, a_2, b_2, c_2, ..., a_N, b_N, c_N]  
    
    in-place using constant extra space.

---
+++ CLang Practice/convertArray.c
@@ -0,0 +1,44 @@
+// A Dynamic Programming based program to check whether a string C is
+// an interleaving of two other strings A and B.
+#include <iostream>
+#include <string.h>
+using namespace std;
+void swapper(int *a,int *b)
+{
+    int temp;
+    temp = *a,
+    *a = *b;
+    *b = temp;
+}
+int get_idx(int cur,int num)
+{
+    return (((cur%3)*num) +(cur/3));
+}
+
+void convertarray(int arr[],int len)
+{
+    int num = len/3;
+    int swap;
+    
+    for(int i = 0; i < len; i++)
+    {
+        swap = get_idx(i,num);
+        while(swap < i)
+            swap = get_idx(swap,num);
+        swapper(&arr[swap],&arr[i]);
+    }
+    
+    for(int i = 0; i < len; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+}
+// Driver program to test above functions
+int main()
+{
+    int arr[] = {1,2,3,4,5,6,7,8,9};
+    int len = sizeof(arr)/sizeof(arr[0]);
+    
+    convertarray(arr,len);
+	return 0;
+}
